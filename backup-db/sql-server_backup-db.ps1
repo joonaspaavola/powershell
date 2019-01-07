9cb45f0e657c3557734c975f0a2d54db1ab74671
Joonas Paavola <joonas.paavola@cgi.com>
2019-01-07 09:26:13 +0200

    Added an SQL db backup script

---
+++ backup-db/sql-server_backup-db.ps1
@@ -0,0 +1,24 @@
+﻿# Provide SQL server details, destination folder and name of the db to backup
+$serverName = "localhost"
+$backupDirectory = Read-Host -Prompt 'Provide destination folder'
+$dbName = Read-Host -Prompt 'Provide database name'
+
+[System.Reflection.Assembly]::LoadWithPartialName(“Microsoft.SqlServer.SMO”) | Out-Null
+[System.Reflection.Assembly]::LoadWithPartialName(“Microsoft.SqlServer.SmoExtended”) | Out-Null
+[System.Reflection.Assembly]::LoadWithPartialName(“Microsoft.SqlServer.ConnectionInfo”) | Out-Null
+[System.Reflection.Assembly]::LoadWithPartialName(“Microsoft.SqlServer.SmoEnum”) | Out-Null
+
+$server = New-Object (“Microsoft.SqlServer.Management.Smo.Server”) $serverName
+$dbs = $server.Databases
+$timestamp = Get-Date -format yyyy-MM-dd-HHmmss
+$targetPath = $backupDirectory + “\” + $dbName + “_” + $timestamp + “.bak”
+$smoBackup = New-Object (“Microsoft.SqlServer.Management.Smo.Backup”)
+$smoBackup.Action = “Database”
+$smoBackup.BackupSetDescription = “Full Backup of “ + $dbName
+$smoBackup.BackupSetName = $dbName + ” Backup”
+$smoBackup.Database = $dbName
+$smoBackup.MediaDescription = “Disk”
+$smoBackup.Devices.AddDevice($targetPath, “File”)
+$smoBackup.SqlBackup($server)
+
+“Backed up $dbName ($serverName) to $targetPath“
